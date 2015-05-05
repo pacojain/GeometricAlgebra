b461f257b530031d81327d4c3138f576c2e9403b
Paco Jain <pacojain@gmail.com>
2015-05-05 10:47:03 -0500

    Added eclipse project and empty package/notebook files.  Added sample code as basis for modification.

---
+++ GeometricAlgebra.m
@@ -0,0 +1,19 @@
+(* ::Package:: *)
+
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench May, 2015 
+   Author: Paco Jain (pacojain@gmail.com)
+*)
+
+BeginPackage["GeometricAlgebra`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+End[]
+
+EndPackage[]
